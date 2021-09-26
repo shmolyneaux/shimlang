8bd49f53c116c70148aef6d198a5bf2eaea82276
Stephen Molyneaux <stephen@molyneaux.ca>
2021-09-25 21:07:08 -0700

    Wire in cli-specified script file

--- src/main.zig
+++ src/main.zig
@@ -10,5 +10,26 @@ pub fn main() !void {
         }
     }
 
-    try libshim.run_text(&gpa.allocator, "print(3 + 3 + 2);");
+    var args = std.process.args();
+
+    _ = args.nextPosix();
+    if (args.nextPosix()) |script_path| {
+        var script_file = std.fs.cwd().openFile(script_path, std.fs.File.OpenFlags{ .read = true }) catch {
+            std.debug.print("Couldn't find file {s}\n", .{script_path});
+            return;
+        };
+        defer script_file.close();
+
+        var five_megabytes: u64 = 5 * 1024 * 1024;
+        var script_text = script_file.readToEndAlloc(&gpa.allocator, five_megabytes) catch {
+            std.debug.print("Error reading file {s}\n", .{script_path});
+            return;
+        };
+        defer gpa.allocator.free(script_text);
+
+        try libshim.run_text(&gpa.allocator, script_text);
+        return;
+    }
+
+    std.debug.print("No script provided\n", .{});
 }
